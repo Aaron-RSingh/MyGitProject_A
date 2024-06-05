f3a00d3db801a24025403f852447bf2d1358ea25
Aaron-RSingh <43779107+Aaron-RSingh@users.noreply.github.com>
2024-06-05 10:09:32 +0100

    python branch

---
+++ test.py
@@ -0,0 +1,15 @@
+#make a quiz with score out of 10 
+print('hello this is my quiz!')
+
+name = input('What is your name? ')
+
+print('Hello ' + name + '. Welcome to the quiz!')
+
+question_list = [
+    'What number is greater than 50 but smaller than 100? ',
+    'What has hands but cannot hold? ',
+    'What is the capital of England? ',
+    ''
+]
+
+print('Testing')
